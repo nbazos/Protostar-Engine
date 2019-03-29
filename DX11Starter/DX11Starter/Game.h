a16dbfd9c54142030cb2f8d0f0de4f7b8596dae0
Nicholas Bazos <nbazos15@gmail.com>
2019-03-29 07:56:04 -0400

    Deleted unecessary comments from Game.h

--- DX11Starter/DX11Starter/Game.h
+++ DX11Starter/DX11Starter/Game.h
@@ -24,31 +24,9 @@ public:
 	void OnMouseMove (WPARAM buttonState, int x, int y);
 	void OnMouseWheel(float wheelDelta,   int x, int y);
 private:
-
-	//// Initialization helper methods - feel free to customize, combine, etc.
-	//void LoadShaders();
-	//void CreateMaterials();
-	//void CreateBasicGeometry();
-
-	//// Wrappers for DirectX shaders to provide simplified functionality
-	//SimpleVertexShader* vertexShader;
-	//SimplePixelShader* pixelShader;
-
-	//Material* material1;
-	//Material* material2;
-	//ID3D11ShaderResourceView* brickSRV;
-	//ID3D11ShaderResourceView* grassSRV;
-	//ID3D11SamplerState* sampler;
-
 	// Keeps track of the old mouse position.  Useful for 
 	// determining how far the mouse moved in a single frame.
 	POINT prevMousePos;
-
-	//std::vector<Mesh*> meshes;
-	//std::vector<GameEntity*> gameEntities;
-	//Camera* camera;
-	//DirectionalLight dirLight1;
-	//DirectionalLight dirLight2;
 
 	// Engine systems
 	EventBus eventBus;
